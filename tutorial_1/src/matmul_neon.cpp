68cd29013ffa1b638243afdfd141ac25e0debc4e
Oliver Grainge <oliver@grainge.me>
2026-02-17 16:14:48 +0000

    reducing matmul size to 4096

--- tutorial_1/src/matmul_neon.cpp
+++ tutorial_1/src/matmul_neon.cpp
@@ -97,7 +97,7 @@ void matmul_neon(const float* A, const float* B, float* C, int N) {
 }
 
 int main(int argc, char* argv[]) {
-    int N = 8192;
+    int N = 4096;
     if (argc > 1) N = std::atoi(argv[1]);
 
     std::vector<float> A(N * N);
